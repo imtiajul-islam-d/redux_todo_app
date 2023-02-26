36bb5c50597b60b569d368644da7ca4265a50ee8
imtiajul-islam-d <imtiajul@hotmail.com>
2023-02-26 12:16:35 +0600

    todo actions added

---
+++ src/redux/todos/action.js
@@ -0,0 +1,51 @@
+import {
+  ADDTODO,
+  ALLCOMPLETED,
+  CLEARCOMPLETED,
+  COLORSELECTED,
+  REMOVETODO,
+  TOGGLETODO,
+} from "./actionTypes";
+
+export const addTask = (task) => {
+  return {
+    type: ADDTODO,
+    payload: task,
+  };
+};
+
+export const deleteTask = (taskId) => {
+  return {
+    type: REMOVETODO,
+    payload: taskId,
+  };
+};
+
+export const toggleTask = (taskId) => {
+  return {
+    type: TOGGLETODO,
+    payload: taskId,
+  };
+};
+
+export const selectColor = (todoId, color) => {
+  return {
+    type: COLORSELECTED,
+    payload: {
+      todoId,
+      color,
+    },
+  };
+};
+
+export const allCompleted = () => {
+  return {
+    type: ALLCOMPLETED,
+  };
+};
+
+export const clearCompleted = () => {
+  return {
+    type: CLEARCOMPLETED,
+  };
+};
---
+++ src/redux/todos/actionTypes.js
@@ -0,0 +1,6 @@
+export const ADDTODO = "todo/addTodo";
+export const REMOVETODO = "todo/removeTodo";
+export const TOGGLETODO = "todo/toggleTodo";
+export const COLORSELECTED = "todo/colorSelected";
+export const ALLCOMPLETED = "todo/allCompleted";
+export const CLEARCOMPLETED = "todo/clearCompleted";
